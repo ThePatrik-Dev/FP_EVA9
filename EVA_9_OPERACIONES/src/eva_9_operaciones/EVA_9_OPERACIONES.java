8f289019cb42c51a8e0be660c141b4d99cfa5fdb
Joel Alderete <thej14653@gmail.com>
2024-09-05 11:14:42 -0600

    Add files via upload

---
+++ EVA_9_OPERACIONES/src/eva_9_operaciones/EVA_9_OPERACIONES.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_9_operaciones;
+
+/**
+ *
+ * @author elmas
+ */
+public class EVA_9_OPERACIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Alderete Aguirre Diego Joel
+        
+      int X = 10;
+      int B = 5;
+      double Y = 5; 
+      double C = 2;
+      double Div;
+      int Suma, Resta, Multiplicacion, Division;
+
+      
+  
+     System.out.println("Los Valores de X = 10 , B = 5 , Y = 5 , C = 2");
+     System.out.println("-------------------------------------------------------");
+      
+     Suma = X + B;
+     System.out.println("El resultado de la suam de X y B: " + Suma);
+     
+     Resta = X - B;
+     System.out.println("El resultado de la resta de X - B: " + Resta);
+     
+     Multiplicacion = X * B;
+     System.out .println("El resultado de la Multiplicacion de X * B: " + Multiplicacion);
+     
+     Division = X / B;
+     System.out.println("La divicion de X / B: " + Division);
+     
+     Div  = Y / C;
+     System.out.println("El resultado de la segunda Div entre C / B: " + Div);
+    }
+    
+}
